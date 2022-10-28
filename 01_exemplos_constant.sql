e08e62f7e891de283a4169f4ccc556b0fe723d4a
Fernando Tulio <fernandotulio@gmail.com>
2022-10-28 10:08:26 -0300

    Constants

---
+++ 01_exemplos_constant.sql
@@ -0,0 +1,12 @@
+ 
+SET SERVEROUTPUT ON
+DECLARE
+  vPi    CONSTANT NUMBER(38,15) := 3.141592653589793; 
+  vCaracterTamFixo     CONSTANT CHAR(2) := 'RS';
+  vCaracterTamVariavel CONSTANT VARCHAR2(100) := 'Porto Alegre, RS';
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('Pi = ' ||   vPi);
+  DBMS_OUTPUT.PUT_LINE('String Caracteres Tam Fixo = ' || vCaracterTamFixo );
+  DBMS_OUTPUT.PUT_LINE('String Caracteres Tam Variável = ' || vCaracterTamVariavel );
+END;
+ 
